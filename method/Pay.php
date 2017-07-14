8b585e99b90efd8b1dd14f15084a3f17008733e8
gizmore <gizmore@wechall.net>
2017-07-14 17:42:25 +0200

    sync

--- method/Pay.php
+++ method/Pay.php
@@ -4,15 +4,15 @@
  * @author gizmore
  * @version 5.0
  */
-final class PaymentCredits_InitPayment extends GWF_MethodPayment
+final class PaymentCredits_Pay extends GWF_Method
 {
 	public function execute()
 	{
-		if (!($order = $this->getOrderPersisted()))
+		if (!($order = GWF_Order::getById(Common::getRequestString('order'))))
 		{
 			return $this->error('err_order');
 		}
-		return $this->renderOrder($order)->add($this->templateButton($order));
+// 		return $this->renderOrder($order)->add($this->templateButton($order));
 	}
 	
 	private function templateButton(GWF_Order $order)
---
+++ tpl/paybutton.php
@@ -0,0 +1,10 @@
+<?php $order instanceof GWF_Order; ?>
+<?php
+$user = $order->getUser();
+$bar = GDO_Bar::make();
+$price = $order->getPrice();
+$module = Module_PaymentCredits::instance();
+$button = GDO_Button::make()->label('btn_pay_credits', [$module->priceToCredits($price), $user->getCredits()]);
+$button->href(href('PaymentCredits', 'Pay', '&order='.$order->getID()));
+$bar->addField($button);
+echo $bar->renderCell();
